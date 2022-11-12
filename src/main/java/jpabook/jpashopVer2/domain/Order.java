7ef9a957d32c9926d524d91aba75a53e6dd78d76
DevelopJKong <ybaram40@gmail.com>
2022-11-12 20:07:59 +0900

    엔티티 클래스 개발1 완료

--- src/main/java/jpabook/jpashopVer2/domain/Order.java
+++ src/main/java/jpabook/jpashopVer2/domain/Order.java
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,11 +30,14 @@ public class Order {
   @JoinColumn(name = "member_id")
   private Member member;
 
+  @OneToMany(mappedBy = "order")
   private List<OrderItem> orderItems = new ArrayList<>();
 
+  @OneToOne
+  @JoinColumn(name = "delivery_id")
   private Delivery delivery;
 
-  private LocalDateTime orderDate;
+  private LocalDateTime orderDate; // 주문시간
 
-  private OrderStatus status; //[ORDER, CANCEL]
+  private OrderStatus status; //주문상태  [ORDER, CANCEL]
 }
