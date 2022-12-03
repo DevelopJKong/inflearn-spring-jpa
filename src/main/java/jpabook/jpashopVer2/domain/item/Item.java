2f1af0e19d6622ad443081cbd99ffdc8dc7e9bc4
cafe small house <ybaram40@gmail.com>
2022-12-03 20:52:14 +0900

    상품 엔티티 개발(비즈니스 로직 추가) 완료

--- src/main/java/jpabook/jpashopVer2/domain/item/Item.java
+++ src/main/java/jpabook/jpashopVer2/domain/item/Item.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+
+import jpabook.jpashopVer2.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +27,27 @@ public class Item {
   private int price;
 
   private int stockQuantity;
+
+  // ==비즈니스 로직==//
+  /**
+   * *stock 증가
+   * 
+   * @param quantity
+   */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * *stock 감소
+   * 
+   * @param quantity
+   */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if(restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
